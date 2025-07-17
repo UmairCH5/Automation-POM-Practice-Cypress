3c952ca47f47352cc57b09451ae563ef52a5f3b3
UmairCH5 <umairch.qa@gmail.com>
2025-07-17 22:08:01 +0500

    Signed-off-by: UmairCH5 <umairch.qa@gmail.com>

--- cypress/e2e/test/orange-login.cy.js
+++
@@ -1,7 +0,0 @@
-describe('magento cart', () =>{
-    it('cart function', () =>{
-        cy.visit("https://opensource-demo.orangehrmlive.com/")
-        cy.get('.oxd-text--h5').should('have.text', 'Login')
-        
-    })
-})
